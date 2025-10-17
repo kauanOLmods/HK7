7469cfacce9efc70b95c9835a72e16ff82f32f36
HK7 <kazan777ss@gmail.com>
2025-10-17 01:14:07 -0300

    Update script.js.js

--- script.js.js
+++ script.js.js
@@ -1,9 +1,9 @@
-// Pega o botão e o áudio
 const playButton = document.getElementById('play-sound');
 const sound = document.getElementById('background-sound');
 
-// Quando o usuário clicar, toca o áudio
+
 playButton.addEventListener('click', () => {
-  sound.play();               // inicia a música
-  playButton.style.display = 'none'; // esconde o botão
+  sound.play();              
+  playButton.style.display = 'none'; 
+
 });
