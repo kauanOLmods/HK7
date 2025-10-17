3032e4709b46f09527f23f347428e58b6a978e40
HK7 <kazan777ss@gmail.com>
2025-10-17 01:19:08 -0300

    Delete script.js.js

--- script.js.js
+++
@@ -1,9 +0,0 @@
-const playButton = document.getElementById('play-sound');
-const sound = document.getElementById('background-sound');
-
-
-playButton.addEventListener('click', () => {
-  sound.play();              
-  playButton.style.display = 'none'; 
-
-});
